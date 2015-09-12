d1e3468070553bc2632d370459b353cc5d88157f
galinka-zch <galinka_zch@mail.ru>
2015-09-12 13:58:04 +0300

    'prog-zadanieR3'

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Галина on 12.09.2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, galinka-zch!");
+    }
+}
